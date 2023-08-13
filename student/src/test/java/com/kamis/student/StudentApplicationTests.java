55abc78b4e9c3ab4f3911e85de6c4f9f1da4b1d7
Elspinoza <inolatse@gmail.com>
2023-08-13 21:14:44 +0000

    Initial first commit

---
+++ student/src/test/java/com/kamis/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamis.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
